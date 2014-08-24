a4f874119522008da76240f8180bee5add24ad3b
mantvydas <mantvydas@localhost>
2014-08-24 18:18:40 +0100

    method for retrieving playlist tracklist

--- apis/app/controllers/FavouritesController.php
+++ apis/app/controllers/FavouritesController.php
@@ -7,4 +7,8 @@ class FavouritesController extends BaseController {
         return DB::select('SELECT * FROM ns_playlists WHERE user_id = ?', [$id]);
 	}
 
+    public function getPlaylistTracklist($id) {
+        return DB::select('SELECT * FROM ns_playlists WHERE user_id = ?', [$id]);
+	}
+
 }
