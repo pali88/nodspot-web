db481061a376964c2969f5eabea2b7847956709f
mantvydas <mantvydas@localhost>
2014-08-25 16:04:49 +0100

    route - existing user?

--- apis/app/controllers/UsersController.php
+++ apis/app/controllers/UsersController.php
@@ -3,14 +3,6 @@
 
 class UsersController extends BaseController {
 
-    public function getPlaylists($id) {
-        return DB::select('SELECT * FROM ns_playlists WHERE user_id = ?', [$id]);
-    }
-
-    public function getFavourites($id) {
-        return DB::select('SELECT * FROM ns_saved_albums WHERE user_id = ?', [$id]);
-    }
-
     public function isExisting($fb_id, $email) {
         $user_id = DB::select('SELECT id FROM ns_users WHERE user_id = ? AND email = ?', [$fb_id, $email]);
 
@@ -22,4 +14,13 @@ class UsersController extends BaseController {
             return DB::getPdo()->lastInsertId();
         }
     }
+
+    public function getPlaylists($id) {
+        return DB::select('SELECT * FROM ns_playlists WHERE user_id = ?', [$id]);
+    }
+
+    public function getFavourites($id) {
+        return DB::select('SELECT * FROM ns_saved_albums WHERE user_id = ?', [$id]);
+    }
+
 }
