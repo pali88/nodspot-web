e96dcc55266565f2284b81bfae9d1452f42c2eb6
mantvydas <mantvydas@localhost>
2014-08-25 15:57:27 +0100

    route - existing user?

--- apis/app/controllers/UsersController.php
+++ apis/app/controllers/UsersController.php
@@ -12,6 +12,14 @@ class UsersController extends BaseController {
     }
 
     public function isExisting($fb_id, $email) {
-        return DB::select('SELECT id FROM ns_users WHERE user_id = ? AND email = ?', [$fb_id, $email]);
+        $user_id = DB::select('SELECT id FROM ns_users WHERE user_id = ? AND email = ?', [$fb_id, $email]);
+
+        if ($user_id) {
+            return $user_id;
+        } else {
+            //adding a new user
+            DB::insert('INSERT INTO ns_users (user_id, email) VALUES (?, ?)', [$fb_id, $email]);
+            return DB::getPdo()->lastInsertId();
+        }
     }
 }
