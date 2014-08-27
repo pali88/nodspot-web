aacb30631fc6de8e57bcd82828910ccc35a8c0f7
mantvydas <mantvydas@localhost>
2014-08-27 21:03:02 +0100

    replaced 'ns_saved_albums' with constants

--- apis/app/controllers/FavouritesController.php
+++ apis/app/controllers/FavouritesController.php
@@ -1,18 +1,20 @@
 <?php
 
+const T_SAVED_ALBUMS = 'ns_saved_albums';
+
 class FavouritesController extends BaseController {
 
 	public function add($user_id, $release_id, $release_type, $search_term, $release_title, $release_year, $artist_name) {
-        DB::insert('INSERT INTO ns_saved_albums (user_id, id, type, search_term, title, year, artist_name)
+        DB::insert('INSERT INTO ' . T_SAVED_ALBUMS . ' (user_id, id, type, search_term, title, year, artist_name)
         VALUES (?, ?, ?, ?, ?, ?, ?)', [$user_id, $release_id, $release_type, $search_term, $release_title, $release_year, $artist_name]);
         return DB::getPDO()->lastInsertId();
 	}
 
     public function delete($user_id, $release_id) {
-        return DB::delete('DELETE FROM ns_saved_albums WHERE (user_id = ? AND id = ?)', [$user_id, $release_id]);
+        return DB::delete('DELETE FROM ' . T_SAVED_ALBUMS . ' WHERE (user_id = ? AND id = ?)', [$user_id, $release_id]);
 	}
 
     public function isExisting($user_id, $release_id) {
-        return DB::select('SELECT id FROM ns_saved_albums WHERE user_id = ? AND id = ?', [$user_id, $release_id]);
+        return DB::select('SELECT id FROM ' . T_SAVED_ALBUMS . ' WHERE user_id = ? AND id = ?', [$user_id, $release_id]);
 	}
 }
