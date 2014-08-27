6e7cdfee8517290dbc129cd8d9fbb2116ac3ecf5
mantvydas <mantvydas@localhost>
2014-08-27 21:54:54 +0100

    moved all DB table name constants to global.php to make it accessible from anywhere within the app

--- apis/app/start/global.php
+++ apis/app/start/global.php
@@ -81,7 +81,7 @@ App::down(function()
 require app_path().'/filters.php';
 
 //DB TABLE NAMES
-const T_PLAYLIST_TRACKS = 'ns_playlist_tracks'; //T for table
+const T_PLAYLIST_TRACKS = 'ns_playlist_tracks';
 const T_PLAYLISTS = 'ns_playlists';
 const T_USERS = 'ns_users';
 const T_SAVED_ALBUMS = 'ns_saved_albums';
