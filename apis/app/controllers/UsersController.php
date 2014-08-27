af88d8d053585e574d263cd4e6016b276bddd451
mantvydas <mantvydas@localhost>
2014-08-27 21:51:02 +0100

    replaced ns_users with constant T_USERS

--- apis/app/controllers/UsersController.php
+++ apis/app/controllers/UsersController.php
@@ -1,26 +1,27 @@
 <?php
 
+const T_USERS = 'ns_users';
 
 class UsersController extends BaseController {
 
     public function isExisting($fb_id, $email) {
-        $user_id = DB::select('SELECT id FROM ns_users WHERE user_id = ? AND email = ?', [$fb_id, $email]);
+        $user_id = DB::select('SELECT id FROM ' . T_USERS . ' WHERE user_id = ? AND email = ?', [$fb_id, $email]);
 
         if ($user_id) {
             return $user_id;
         } else {
             //adding a new user
-            DB::insert('INSERT INTO ns_users (user_id, email) VALUES (?, ?)', [$fb_id, $email]);
+            DB::insert('INSERT INTO ' . T_USERS . ' (user_id, email) VALUES (?, ?)', [$fb_id, $email]);
             return DB::getPdo()->lastInsertId();
         }
     }
 
     public function getPlaylists($id) {
-        return DB::select('SELECT * FROM ns_playlists WHERE user_id = ?', [$id]);
+        return DB::select('SELECT * FROM ' . T_PLAYLISTS . ' WHERE user_id = ?', [$id]);
     }
 
     public function getFavourites($id) {
-        return DB::select('SELECT * FROM ns_saved_albums WHERE user_id = ?', [$id]);
+        return DB::select('SELECT * FROM ' . T_SAVED_ALBUMS . ' WHERE user_id = ?', [$id]);
     }
 
 }
