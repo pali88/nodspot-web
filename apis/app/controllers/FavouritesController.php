24c9513e25d8affbe6e2a76f94285ff565136721
mantvydas <mantvydo@gmail.com>
2014-09-15 22:37:37 +0100

    retrieves user_id from header.hash

--- apis/app/controllers/FavouritesController.php
+++ apis/app/controllers/FavouritesController.php
@@ -3,17 +3,20 @@
 
 class FavouritesController extends BaseController {
 
-	public function add($user_id, $release_id, $release_type, $search_term, $release_title, $release_year, $artist_name) {
+	public function add($release_id, $release_type, $search_term, $release_title, $release_year, $artist_name) {
+        $user_id = UsersController::getUserByHash();
         DB::insert('INSERT INTO ' . T_SAVED_ALBUMS . ' (user_id, id, type, search_term, title, year, artist_name)
         VALUES (?, ?, ?, ?, ?, ?, ?)', [$user_id, $release_id, $release_type, $search_term, $release_title, $release_year, $artist_name]);
         return DB::getPDO()->lastInsertId();
 	}
 
-    public function delete($user_id, $release_id) {
+    public function delete($release_id) {
+        $user_id = UsersController::getUserByHash();
         return DB::delete('DELETE FROM ' . T_SAVED_ALBUMS . ' WHERE (user_id = ? AND id = ?)', [$user_id, $release_id]);
 	}
 
-    public function isExisting($user_id, $release_id) {
+    public function isExisting($release_id) {
+        $user_id = UsersController::getUserByHash();
         return DB::select('SELECT id FROM ' . T_SAVED_ALBUMS . ' WHERE user_id = ? AND id = ?', [$user_id, $release_id]);
 	}
 }
--- apis/app/controllers/LogController.php
+++ apis/app/controllers/LogController.php
@@ -2,13 +2,8 @@
 
 class LogController extends BaseController {
 
-    public function logSearch($user_id, $search_term, $search_type) {
-         DB::insert('INSERT INTO ns_searches (user_id, search_term, type) VALUES (?, ?, ?)', [$user_id, $search_term, $search_type]);
-
-// if (isset($user_id_nodspot)) {
-//            $user_id = $user_id_nodspot;
-//        } else {
-//            $user_id = 0;
-//        }
+    public function logSearch($search_term, $search_type) {
+        $user_id = UsersController::getUserByHash();
+        DB::insert('INSERT INTO ns_searches (user_id, search_term, type) VALUES (?, ?, ?)', [$user_id, $search_term, $search_type]);
     }
 }
