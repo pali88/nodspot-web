cc8fa89e566668d71dc9bb832a339e9fdf5a0b4a
mantvydas <mantvydo@gmail.com>
2014-09-17 23:54:41 +0100

    reorganised routes

--- apis/app/routes.php
+++ apis/app/routes.php
@@ -15,25 +15,24 @@ Route::get('/', function () {
     return 'Welcome back, Mantvydas';
 });
 
-Route::get('/pienas', function () {
-    return Session::all();
-});
+//testing purposes
+Route::resource('getUserByHash', 'UsersController@getUserByHash');
 
 Route::resource('user', 'UsersController');
 Route::resource('user/{fb_id}/{email}/existing', 'UsersController@isExisting');
-Route::resource('favourites', 'UsersController@getFavourites');
-Route::resource('playlists', 'UsersController@getPlaylists');
 
+Route::resource('playlists', 'PlaylistsController@getPlaylists');
 Route::resource('playlist', 'PlaylistsController');
 Route::resource('playlist/{playlist_name}/existing', 'PlaylistsController@isExisting');
 Route::resource('playlist/{playlist_name}/new', 'PlaylistsController@createPlaylist');
 Route::resource('playlist/{playlist_id}/delete', 'PlaylistsController@deletePlaylist');
 Route::resource('playlist/{playlist_id}/{{new_playlist_name}/update', 'PlaylistsController@updatePlaylist');
-
 Route::resource('playlist/{playlist_id}/{track_id}/{track_title}/add', 'PlaylistsController@addTrack');
 Route::resource('playlist/{playlist_id}/{track_id}/remove', 'PlaylistsController@removeTrack');
+
 Route::resource('track/{track_id}/belongsTo', 'PlaylistsController@trackBelongsTo');
 
+Route::resource('favourites', 'FavouritesController@getFavourites');
 Route::resource('favourites/{release_id}/{type}/{search_term}/{title}/{year}/{artist_name}/add', 'FavouritesController@add');
 Route::resource('favourites/{release_id}/delete', 'FavouritesController@delete');
 Route::resource('favourites/{release_id}/existing', 'FavouritesController@isExisting');
