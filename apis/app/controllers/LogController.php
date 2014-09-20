1add44f8c0e023e432b116e4303d9470e5a4c689
mantvydas <mantvydo@gmail.com>
2014-09-20 20:30:32 +0100

    returns 0 for undefined users instead of NULL

--- apis/app/controllers/LogController.php
+++ apis/app/controllers/LogController.php
@@ -4,6 +4,7 @@ class LogController extends BaseController {
 
     public function logSearch($searchTerm, $searchType) {
         $userId = UsersController::getUserIdByHash();
+        $userId = ($userId) ? $userId : 0; //if no user is returned, the request will be logged as if the user_id = 0 was making the request.
         DB::insert('INSERT INTO ns_searches (user_id, search_term, type) VALUES (?, ?, ?)', [$userId, $searchTerm, $searchType]);
     }
 }
